e5af42c2c292cdc86349671e092ab54326b329cf
Himanshu-K06 <himanshu952511@gmail.com>
2025-12-19 20:37:22 +0530

    cleaning code

--- snake.py
+++ snake.py
@@ -2,8 +2,6 @@ import pygame
 import sys, random
 from pygame.math import Vector2
 
-# definitions (basic set of the game)  : 
-#_______definitions of the game starts __________________________________________________
 pygame.init()  #initialize pygame 
 title_font = pygame.font.Font(None,40)
 score_font = pygame.font.Font(None,40)
@@ -16,16 +14,14 @@ DARK_BLUE = (19,30,58)
 cell_size = 20
 number_of_cells = 30
 
-OFFSET = 50 # offset value will determine the width of the border 
+OFFSET = 50 
 
 class Food: 
-    def __init__(self,snake_body):  # init() method 
-        # use vector2 class, pygame lib offers it but import it from pygame.math 
-        self.position =  self.generate_random_pos(snake_body)  #(x,y) coordinates of the food , x and y both start from 0 
+    def __init__(self,snake_body):  
+        self.position =  self.generate_random_pos(snake_body)  
     
-    def draw(self): #food_rect = pygame.Rect(x,y,w,h) -- w and h are width and height - x,y coordinates
+    def draw(self): 
         food_rect = pygame.Rect(OFFSET+self.position.x*cell_size, OFFSET+self.position.y*cell_size, cell_size, cell_size)
-        #pygame.draw.rect(screen,DARK_BLUE,food_rect)  # pygame.draw.rect(surface,color,rect)
         screen.blit(food_surface,food_rect)
 
     def generate_random_cell(self):
@@ -41,13 +37,13 @@ class Food:
 
 class Snake: 
     def __init__(self):
-        self.body = [Vector2(6,9),Vector2(5,9),Vector2(4,9)]  # the initial snake with length equal to only 3 blocks with positions as mentioned 
+        self.body = [Vector2(6,9),Vector2(5,9),Vector2(4,9)]  
         self.direction = Vector2(1,0)
         self.add_segment = False
 
     def draw(self):
         for segment in self.body:
-            segment_rect = (OFFSET+segment.x*cell_size, OFFSET+segment.y*cell_size ,cell_size,cell_size) # create rectangle for each segment in the body 
+            segment_rect = (OFFSET+segment.x*cell_size, OFFSET+segment.y*cell_size ,cell_size,cell_size) 
             pygame.draw.rect(screen,DARK_BLUE,segment_rect,0,9)
 
     def update(self): # update the position of snake body
@@ -55,12 +51,11 @@ class Snake:
         if self.add_segment == True:
             self.add_segment  = False
         else: 
-            self.body = self.body[:-1]  # what the fuck just happened ???????????????????????????????????
-        # reassigning the whole snake body from start to end exxcept the last segemnt  to itself 
+            self.body = self.body[:-1]  
+
     def reset(self):
         self.body= [Vector2(6,9),Vector2(5,9),Vector2(4,9)]
         self.direction = Vector2(1,0)
-        # self.food.position = self.food.generate_random_pos(self.snake.body)
 
 class Game:
     def __init__(self):
@@ -104,24 +99,20 @@ class Game:
         if self.snake.body[0] in headless_body:
             self.game_over()
 
-screen = pygame.display.set_mode((2*OFFSET+cell_size*number_of_cells, 2*OFFSET+ cell_size*number_of_cells))  # takes a tuple as an argument (width,height)
-# we just made a game window, which uses coordinate system where (0,0) is top left corner and y coordinates won't be in negative when we move down 
+screen = pygame.display.set_mode((2*OFFSET+cell_size*number_of_cells, 2*OFFSET+ cell_size*number_of_cells))  
 
 pygame.display.set_caption("Snake Game 🐍")
 #clock object 
-clock = pygame.time.Clock()  # controls the frame rate of the game (how fast it runs )
-# definitions of the game ends -----------------------------------------------------------
+clock = pygame.time.Clock() 
 
 game = Game()
 food_surface = pygame.image.load("graphics/food_image.jpeg")
-# GAME LOOP :  event handling ---->> updating positions ----->> drawing objects 
-#_________GAME LOOP STARTS __________________________________________________________________
+
 SNAKE_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SNAKE_UPDATE,225)  # 200 here is time in miliseconds
-## 1. EVENT HANDLING  : 
+
 while True:
-    for event in pygame.event.get(): #get all the events which pygame recognizes and happened -> put in list
-        # look through the list of events and check if we have any QUIT event 
+    for event in pygame.event.get(): 
         if event.type  == SNAKE_UPDATE: 
             game.update()
         if event.type == pygame.QUIT:
@@ -130,7 +121,7 @@ while True:
         if event.type == pygame.KEYDOWN:
             if game.state == "STOPPED": 
                 game.state = "RUNNING"
-            if event.key == pygame.K_UP and game.snake.direction!= Vector2(0,1): # checks if the player has pressed up key or not
+            if event.key == pygame.K_UP and game.snake.direction!= Vector2(0,1): 
                 game.snake.direction = Vector2(0,-1)
             if event.key == pygame.K_DOWN and game.snake.direction!= Vector2(0,-1):
                 game.snake.direction = Vector2(0,1)
@@ -141,25 +132,14 @@ while True:
 
     screen.fill(BLUE)
     game.draw()
-    pygame.draw.rect(screen,DARK_BLUE,(OFFSET-5,OFFSET-5, cell_size*number_of_cells+10, cell_size*number_of_cells+10),5)  #(surface,color,rect,border_size)
+    pygame.draw.rect(screen,DARK_BLUE,(OFFSET-5,OFFSET-5, cell_size*number_of_cells+10, cell_size*number_of_cells+10),5)  
     
     title_surface = title_font.render("Retro Snake Game",True,DARK_BLUE)
     score_surface = score_font.render(str(game.score),True,DARK_BLUE)
     screen.blit(title_surface,(OFFSET-5,20))
     screen.blit(score_surface,(OFFSET-5,OFFSET+cell_size*number_of_cells+10))
 
-    pygame.display.update()  # update postions based on the input we gave 
-    clock.tick(60) #tell the clock object how fast the game runs using tick method , takes an integer as an arg, that integer is the number of frames per second --->> 60 frames per second 
+    pygame.display.update()  
+    clock.tick(60) 
 
-# the while loop and all the code inside it runs 60 times per second 
-
-#GAME LOOP ENDS -------------------------------------------------------------------------------
-
-
-# colors are represented as values in tuples , representing the value of red green and blue 
-
-
-#############################################################################################################
-
-# making food 
-# first create a grid -- food is one cell and snake is lot of cells in a sequence 
+    
